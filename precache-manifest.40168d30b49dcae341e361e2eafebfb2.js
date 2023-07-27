2cb3ca3a0d970834b037c36a596b5e388cad5289
kyght1 <nicolas.cartes@usach.cl>
2023-07-26 21:07:16 -0700

    Updates

---
+++ precache-manifest.40168d30b49dcae341e361e2eafebfb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e9e54dde5581a1c120a",
+    "url": "/TareaFinal/static/css/main.3a64a44d.chunk.css"
+  },
+  {
+    "revision": "4e9e54dde5581a1c120a",
+    "url": "/TareaFinal/static/js/main.4e9e54dd.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/TareaFinal/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "2d95bfaac219ff3e3bcb",
+    "url": "/TareaFinal/static/js/2.2d95bfaa.chunk.js"
+  },
+  {
+    "revision": "171a0c42dbff17785657",
+    "url": "/TareaFinal/static/js/runtime~main.171a0c42.js"
+  },
+  {
+    "revision": "431615481007f413f10a9864dba3800a",
+    "url": "/TareaFinal/static/media/LOGO_ai_rgb-01.43161548.png"
+  },
+  {
+    "revision": "ff3ebb04c89db6bbb5115d42e29109b0",
+    "url": "/TareaFinal/index.html"
+  }
+];
